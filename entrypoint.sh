0f0630132d1cc5e74cd58f3f117547d30af3002a
Vittor Faria Pereira <vittorfpereira@gmail.com>
2022-04-14 18:21:28 -0300

    fixing docker image

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,13 +9,16 @@ git config --global user.name "$INPUT_USER_NAME"
 # clone grypi
 git clone --single-branch --branch master "https://x-access-token:$API_TOKEN_GITHUB@github.com/ow-gryphon/grypi.git" "grypi"
 
+# clone actions
+git clone --single-branch --branch master "https://x-access-token:$API_TOKEN_GITHUB@github.com/ow-gryphon/update-grypy-action.git" "actions"
+
 # clone the template repo
 git clone --single-branch --branch master "https://x-access-token:$API_TOKEN_GITHUB@github.com/${GITHUB_REPOSITORY}.git" "template"
 
 # run the python files
 pip install beautifulsoup4
-python src/update_index_html.py
-python src/update_metadata_json.py
+python actions/src/update_index_html.py
+python actions/src/update_metadata_json.py
 
 # send to index
 #   clone destination repo (index)
@@ -32,7 +35,7 @@ cd "destination"
 git add .
 if git status | grep -q "Changes to be committed"
 then
-  git commit --message "Update from https://github.com/${INPUT_GITHUB_REPOSITORY}/commit/${GITHUB_SHA}"
+  git commit --message "Update from https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}"
   echo "Pushing git commit"
   git push -u origin HEAD:master
 else
